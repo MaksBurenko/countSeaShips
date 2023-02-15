2251223ad1c35f191ca7e241fc9b5b78d8b6224d
Maks Burenko <Cherniymaks@yandex.ru>
2023-02-15 16:43:15 +0300

    123

--- src/test/java/CountSeaShipsTest.java
+++ src/test/java/CountSeaShipsTest.java
@@ -1,16 +1,13 @@
-import junit.framework.TestCase;
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CountSeaShipsTest extends TestCase {
+public class CountSeaShipsTest {
 
     int[][] matrix = {{1,0,0,1,0},{1,0,0,1,0},{1,0,0,0,0},{0,0,1,1,1},{1,0,0,0,0}};
 
     @Test
     public void testShouldCountShips() {
-        assertThat(CountSeaShips.count(matrix)).is(4);
+        assertThat(CountSeaShips.count(matrix)).isEqualTo(4);
     }
 
 }
